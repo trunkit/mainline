27567c0a8ebae584c8540e74ecc633d2efcd8cb0
William Weidendorf <wweidendorf@gmail.com>
2013-04-09 20:17:22 -0400

    Initial rails framework structure

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Swayly::Application.config.secret_key_base = 'dc584076112a0c687a96e31868b4391b3a65bf3f4bdfc7689b81bd57f573ad3ca1b0980d433b5adcfdaba4220aca94d4d29ab9b34642925f37c1ff5dcaca1257'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+Swayly::Application.config.session_store :cookie_store, key: '_swayly_session'
