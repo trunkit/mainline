e1c5c6c1581cc2000fba3d5a2b470d10aab8b2b5
William Weidendorf <wweidendorf@gmail.com>
2013-04-13 22:21:24 -0400

    Configure s3 credentials

--- config/configatron/defaults.rb
+++ config/configatron/defaults.rb
@@ -3,5 +3,8 @@
 # Example:
 #   configatron.emails.welcome.subject = 'Welcome!'
 #   configatron.emails.sales_reciept.subject = 'Thanks for your order'
-# 
+#
 #   configatron.file.storage = :s3
+
+configatron.s3.access_key_id     = "AKIAIVXNOE4PA6LP7BJQ"
+configatron.s3.secret_access_key = "5cp59VhNKi7T+c5jSxz6A0r7F98Ld0uqVd/dldNw"
