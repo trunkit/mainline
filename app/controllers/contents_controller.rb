2f0d7e67e7f7112b5b37a79c85a76d4111826ca9
William Weidendorf <wweidendorf@gmail.com>
2013-04-13 20:08:34 -0400

    Don't need extra forward slash at beginning of path

--- app/controllers/contents_controller.rb
+++ app/controllers/contents_controller.rb
@@ -1,6 +1,6 @@
 class ContentsController < ApplicationController
   def show
-    path = (request.path == "/" ? "index" : request.path)
+    path = (request.path == "/" ? "index" : request.path[1,request.path.length-1])
     path = path.downcase.gsub(/[^A-Za-z0-9\/]+/, '_')
 
     render(template: "contents/#{path}")
