363d1ba8c43c6cb3dc7edf5736fae33432f17a36
William Weidendorf <wweidendorf@gmail.com>
2013-04-13 22:21:30 -0400

    Added asset_sync

--- Gemfile
+++ Gemfile
@@ -16,6 +16,7 @@ gem 'sass-rails',   '~> 4.0.0.beta1'
 gem 'uglifier',     '~> 1.3'
 gem 'coffee-rails', '~> 4.0.0.beta1'
 gem 'jquery-rails'
+gem 'asset_sync'
 
 # Monitoring services
 gem 'airbrake'
---
+++ config/initializers/asset_sync.rb
@@ -0,0 +1,15 @@
+if defined?(AssetSync)
+  AssetSync.configure do |config|
+    config.fog_provider = 'AWS'
+    config.fog_directory = 'swayly-mainline'
+    # These can be found under Access Keys in AWS Security Credentials
+    config.aws_access_key_id = configatron.s3.access_key_id
+    config.aws_secret_access_key = configatron.s3.secret_access_key
+
+    # Don't delete files from the store
+    config.existing_remote_files = 'keep'
+
+    # Automatically replace files with their equivalent gzip compressed version
+    config.gzip_compression = true
+  end
+end
