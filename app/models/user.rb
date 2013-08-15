54618d89df6295ba4445a6722f8e38ded7462f7d
William Weidendorf <wweidendorf@gmail.com>
2013-08-15 16:35:50 -0400

    Added roles to users

--- app/models/user.rb
+++ app/models/user.rb
@@ -11,9 +11,25 @@ class User
   field :t,  as: :twitter_id,      type: String
   field :f,  as: :facebook_id,     type: String
 
+  field :r,  as: :role,            type: Symbol
+
   has_secure_password
+
+  cattr_reader :roles
+  @@roles = [:shopper, :boutique, :brand, :admin]
+
+  before_save :validate_role
 
   def self.from_session(sess)
     User.where("_id" => sess["user_id"]).first
+  end
+
+  private
+
+  def validate_role
+    if !self.class.roles.include?(role)
+      errors.add(:role, "is invalid")
+      false
+    end
   end
 end
