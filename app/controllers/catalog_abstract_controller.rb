4f1cf3c5b6577a83b74dd5a1ed4fcc5c3b491cf9
William Weidendorf <wweidendorf@gmail.com>
2013-08-21 13:06:00 -0400

    State confusion

--- app/controllers/catalog_abstract_controller.rb
+++ app/controllers/catalog_abstract_controller.rb
@@ -5,6 +5,6 @@ class CatalogAbstractController < ApplicationController
 
   def check_user_state
     redirect_to(root_path) unless current_user
-    redirect_to(categories_user_path(user_type: current_user.role)) if current_user.ready?
+    redirect_to(categories_user_path(user_type: current_user.role)) unless current_user.ready?
   end
 end
