b1dffe4ce5f8a6ddf2d8a152253300332cd7cd1b
William Weidendorf <wweidendorf@gmail.com>
2013-08-21 14:17:49 -0400

    On Shopper create, redirect to discover_path

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,20 +1,17 @@
 class UsersController < ApplicationController
   layout "home"
 
-  def new
-    @user = User.new(role: params[:user_type])
-    render(template: "users/new_#{@user.role}")
-  end
-
   def create
     @user      = User.new(user_params)
     @user.role = params[:user_type]
 
     if @user.save
       session["user_id"] = @user.id
-      redirect_to(categories_user_path(user_type: @user.role))
-    else
-      render(template: "users/new_#{@user.role}")
+
+      case @user.role
+      when :shopper
+        redirect_to(discover_path)
+      end
     end
   end
 
