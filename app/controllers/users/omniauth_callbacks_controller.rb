1471d7d17ad892a91eecd3925fbccc4093b618e0
William Weidendorf <wweidendorf@gmail.com>
2013-12-02 00:00:37 -0500

    On failure to create user from FB or Twitter, redirect to the homepage

--- app/controllers/users/omniauth_callbacks_controller.rb
+++ app/controllers/users/omniauth_callbacks_controller.rb
@@ -6,7 +6,7 @@ class Users::OmniauthCallbacksController < Devise::OmniauthCallbacksController
       sign_in_and_redirect @user, event: :authentication
     else
       session["devise.facebook_data"] = request.env["omniauth.auth"]
-      redirect_to(new_user_registration_url)
+      redirect_to(root_path)
     end
   end
 
@@ -17,7 +17,7 @@ class Users::OmniauthCallbacksController < Devise::OmniauthCallbacksController
       sign_in_and_redirect @user, event: :authentication
     else
       session["devise.twitter_data"] = request.env["omniauth.auth"]
-      redirect_to(new_user_registration_url)
+      redirect_to(root_path)
     end
   end
 end
