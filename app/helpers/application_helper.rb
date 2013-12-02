56464c0fc0826b6ad60ee7c35209b3c3046c83c1
William Weidendorf <wweidendorf@gmail.com>
2013-12-01 23:02:39 -0500

    No longer have role, but parent on user

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,5 +1,10 @@
 module ApplicationHelper
   def main_nav(hide_main_nav = false)
-    render("layouts/#{current_user.role}_nav") unless hide_main_nav
+    if !hide_main_nav
+      nav_type = current_user.parent_type.downcase
+      nav_type = "user" if nav_type.blank?
+
+      render("layouts/#{current_user.role}_nav")
+    end
   end
 end
