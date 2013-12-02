cceb6b84b0263380abe3d0dd8edbdaf173c2ea7f
William Weidendorf <wweidendorf@gmail.com>
2013-12-02 00:51:27 -0500

    Set twitter in development

--- config/configatron/development.rb
+++ config/configatron/development.rb
@@ -7,3 +7,8 @@ configatron.facebook do |f|
   f.id     = "159347764265403"
   f.secret = "b27a5e0c8e0c1c8a118acfb1e92875bc"
 end
+
+configatron.twitter do |t|
+  t.consumer_key    = "7xML0J1SPrJyO2DHeE1EA"
+  t.consumer_secret = "qi25RIMVgzlow9cGfh2dWUz0MbO4cI2kvBxWV42ig"
+end
