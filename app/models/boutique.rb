f2c0998c7ec65cdd56beb0a7bd0fa96d52f35b3f
William Weidendorf <wweidendorf@gmail.com>
2013-12-02 22:35:04 -0500

    Generate boutique short_code if one isn't provided

--- app/models/boutique.rb
+++ app/models/boutique.rb
@@ -1,4 +1,13 @@
 class Boutique < ActiveRecord::Base
   has_many :locations, as: :company
   has_many :users, as: :parent
+
+  before_create :generate_short_code
+
+  validates_uniqueness_of :short_code
+
+private
+  def generate_short_code
+    self.short_code = (0...6).map{ ('a'..'z').to_a[rand(26)] }.join if short_code.blank?
+  end
 end
