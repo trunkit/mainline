2c7969b482b23e739c3e2023b5b4b971576111bd
William Weidendorf <wweidendorf@gmail.com>
2013-12-02 22:17:25 -0500

    Use cache store for sessions

--- config/initializers/session_store.rb
+++ config/initializers/session_store.rb
@@ -1,3 +1,3 @@
 # Be sure to restart your server when you modify this file.
-
-Trunkit::Application.config.session_store :cookie_store, key: '_trunkit_session'
+#
+Trunkit::Application.config.session_store :cache_store
