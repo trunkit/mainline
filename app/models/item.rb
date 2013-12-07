355d4f0df616bb5390ba95c7f245179273891b96
William Weidendorf <wweidendorf@gmail.com>
2013-12-07 13:59:05 -0500

    Cache count of items on boutique

--- app/models/item.rb
+++ app/models/item.rb
@@ -1,6 +1,6 @@
 class Item < ActiveRecord::Base
   belongs_to :parent, class_name: "Item"
-  belongs_to :boutique
+  belongs_to :boutique, counter_cache: true
 
   has_many :options, class_name: "ItemOptions"
 end
