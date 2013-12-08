4314af6d8ab5222d1086e9a54d72187f182c6875
William Weidendorf <wweidendorf@gmail.com>
2013-12-08 13:52:53 -0500

    Account for the number of states that the main nav can be in

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,14 +1,14 @@
 module ApplicationHelper
-  def main_nav(hide_main_nav = false)
-    if !hide_main_nav
-      if current_user
-        type = current_user.parent_type.try(:downcase)
-        type = "user" if type.blank?
+  def main_nav
+    return render("layouts/public_nav") unless current_user
 
-        render("layouts/#{type}_nav")
-      else
-        render("layouts/public_nav")
-      end
+    if current_user.has_role?(:system)
+      render("layouts/system_nav")
+    else
+      type = current_user.parent_type.try(:downcase)
+      type = "user" if type.blank?
+
+      render("layouts/#{type}_nav")
     end
   end
 end
