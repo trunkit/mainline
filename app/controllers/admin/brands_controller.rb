66f428d776b7ed70b712cd3969df26b5d1bbe26e
William Weidendorf <wweidendorf@gmail.com>
2013-12-08 18:53:21 -0500

    Added basic brands admin interface for JSON responses

--- app/controllers/admin/brands_controller.rb
+++ app/controllers/admin/brands_controller.rb
@@ -1,2 +1,26 @@
 class Admin::BrandsController < Admin::AbstractController
+  def index
+    @brands = Brand.order("name")
+
+    respond_to do |format|
+      format.html
+      format.json { render(json: @brands) }
+    end
+  end
+
+  def create
+    brand = Brand.create(brand_params)
+    return render nothing: true, status: 200
+
+    respond_to do |format|
+      format.html { redirect_to([:edit, :admin, brand]) }
+      format.json { render(json: brand) }
+    end
+  end
+
+  private
+
+  def brand_params
+    params.require(:brand).permit([:name])
+  end
 end
