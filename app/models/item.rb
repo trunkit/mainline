026008111ea2f026c8e2a6f684d33af41c6bee58
William Weidendorf <wweidendorf@gmail.com>
2013-12-08 18:59:29 -0500

    Added brand association to Item

--- app/models/item.rb
+++ app/models/item.rb
@@ -4,6 +4,7 @@ class Item < ActiveRecord::Base
 
   belongs_to :parent, class_name: "Item"
   belongs_to :boutique, counter_cache: true
+  belongs_to :brand
 
   has_many :options, class_name: "ItemOptions"
 end
