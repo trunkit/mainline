b588dce187be742b200d67d08597393f1331b3a8
William Weidendorf <wweidendorf@gmail.com>
2013-12-08 15:01:35 -0500

    Added CartItems table

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20131208184756) do
+ActiveRecord::Schema.define(version: 20131208195245) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -37,6 +37,16 @@ ActiveRecord::Schema.define(version: 20131208184756) do
   end
 
   add_index "boutiques", ["short_code"], name: "index_boutiques_on_short_code", unique: true, using: :btree
+
+  create_table "cart_items", force: true do |t|
+    t.integer  "cart_id",      null: false
+    t.integer  "item_id",      null: false
+    t.integer  "item_version", null: false
+    t.hstore   "item_options"
+    t.datetime "deleted_at"
+    t.datetime "created_at"
+    t.datetime "updated_at"
+  end
 
   create_table "carts", force: true do |t|
     t.integer  "order_id"
