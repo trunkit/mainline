8c56f1c81917018d485285f665b15a90baec5e27
William Weidendorf <wweidendorf@gmail.com>
2013-12-08 17:20:51 -0500

    Remove unused avatar column from user

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20131208214126) do
+ActiveRecord::Schema.define(version: 20131208220937) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -129,7 +129,6 @@ ActiveRecord::Schema.define(version: 20131208214126) do
     t.string   "first_name"
     t.string   "last_name"
     t.string   "gender"
-    t.string   "avatar"
     t.integer  "parent_id"
     t.datetime "created_at"
     t.datetime "updated_at"
