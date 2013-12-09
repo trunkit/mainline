146d9b00ad5e89162496a7bdffa704e6736ee436
William Weidendorf <wweidendorf@gmail.com>
2013-12-08 21:14:35 -0500

    Don't render out each asset file individually anymore

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -22,7 +22,7 @@ Trunkit::Application.configure do
   # Debug mode disables concatenation and preprocessing of assets.
   # This option may cause significant delays in view rendering with a large
   # number of complex assets.
-  config.assets.debug = true
+  config.assets.debug = false
 
   # Configure ActionMailer
   config.action_mailer.default_url_options = { host: 'localhost:3000' }
