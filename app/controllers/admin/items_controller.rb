b592a008978fd293b2f3957fe47c3d99620bfeed
William Weidendorf <wweidendorf@gmail.com>
2013-12-08 19:05:14 -0500

    Added initial admin items interface

--- app/controllers/admin/items_controller.rb
+++ app/controllers/admin/items_controller.rb
@@ -1,2 +1,51 @@
 class Admin::ItemsController < Admin::AbstractController
+  def index
+    @items = Item.includes(:boutique, parent: :boutique)
+  end
+
+  def new
+    @item      = Item.new
+    @brands = Brand.order("name")
+    @boutiques = Boutique.order("name")
+  end
+
+  def create
+    @item = Item.create(item_params)
+
+    if @item.errors.blank?
+      redirect_to([:edit, :admin, @item])
+    else
+      @brands = Brand.order("name")
+      @boutiques = Boutique.order("name")
+      render(action: :new)
+    end
+  end
+
+  def edit
+    @item      = Item.find(params[:id])
+    @brands    = Brand.order("name")
+    @boutiques = Boutique.order("name")
+  end
+
+  def update
+    @item = Item.find(params[:id])
+
+    if item.errors.blank?
+      redirect([:edit, :admin, @item])
+    else
+      @brands = Brand.order("name")
+      @boutiques = Boutique.order("name")
+    end
+  end
+
+  def destroy
+    Item.destroy(params[:id])
+    redirect_to(action: :index)
+  end
+
+  private
+
+  def item_params
+    params.require(:item).permit([:name, :price, :description, :fit, :construction, :model_measurements, :brand_id, :boutique_id])
+  end
 end
