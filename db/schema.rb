43aa886582fdb80e07176ac7088f76e3f27f60ef
William Weidendorf <wweidendorf@gmail.com>
2013-12-31 14:06:52 -0500

    Added #quantity to CartItem

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20131231172130) do
+ActiveRecord::Schema.define(version: 20131231190458) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -59,6 +59,7 @@ ActiveRecord::Schema.define(version: 20131231172130) do
     t.datetime "deleted_at"
     t.datetime "created_at"
     t.datetime "updated_at"
+    t.integer  "quantity"
   end
 
   create_table "carts", force: true do |t|
