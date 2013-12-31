5363bf3359f004e88583429d15f9cec65791f98b
William Weidendorf <wweidendorf@gmail.com>
2013-12-31 00:23:34 -0500

    Properly render stream#following

--- app/controllers/stream_controller.rb
+++ app/controllers/stream_controller.rb
@@ -2,4 +2,8 @@ class StreamController < CatalogAbstractController
   def index
     @items = Item.all
   end
+
+	def following
+		@boutiques = Boutique.includes(:top_items, { locations: :address })
+	end
 end
