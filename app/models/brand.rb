060f6bb7af7f5d724582f4e7e684a5ee6e42a56a
William Weidendorf <wweidendorf@gmail.com>
2013-12-30 20:47:35 -0500

    Remove all brand items on destroy

--- app/models/brand.rb
+++ app/models/brand.rb
@@ -2,5 +2,5 @@ class Brand < ActiveRecord::Base
   acts_as_paranoid
   has_paper_trail
 
-	has_many :items, -> { where(parent_id: nil) }
+	has_many :items, -> { where(parent_id: nil) }, dependent: :destroy
 end
