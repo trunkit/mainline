a296f29da08a68d7f0df3cdf1c02b1eba2452935
William Weidendorf <wweidendorf@gmail.com>
2013-12-31 00:19:20 -0500

    Added baseline favorites view

--- app/controllers/favorites_controller.rb
+++ app/controllers/favorites_controller.rb
@@ -1,5 +1,5 @@
 class FavoritesController < ApplicationController
 	def index
-		@items = Item.all
+		@items_by_boutique = Item.includes(boutique: { locations: :address }).group_by(&:boutique)
 	end
 end
