de6560464551878f3520ce31467004eebc9c1e28
William Weidendorf <wweidendorf@gmail.com>
2013-12-30 22:48:06 -0500

    Mounted Uploaders for Cover and Stream photos on Location

--- app/models/location.rb
+++ app/models/location.rb
@@ -1,4 +1,7 @@
 class Location < ActiveRecord::Base
   belongs_to :company, polymorphic: true
   has_one    :address, as: :parent
+
+	mount_uploader :cover_photo,  CoverPhotoUploader
+	mount_uploader :stream_photo, StreamPhotoUploader
 end
