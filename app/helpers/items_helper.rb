8978bd4a991ce1468d35c8b3e6670a408c0db7d8
William Weidendorf <wweidendorf@gmail.com>
2013-12-31 14:43:56 -0500

    Added ability to render out item quantities and options for selection

--- app/helpers/items_helper.rb
+++ app/helpers/items_helper.rb
@@ -1,2 +1,13 @@
 module ItemsHelper
+  def item_options_for_select(group, options)
+    arr = options.map{|o| [o.value, o.id] }
+    arr.unshift(["Select #{group}:", nil])
+
+    options_for_select(arr)
+  end
+
+  def item_quantities
+    arr = (1..10).to_a
+    arr.unshift(["Qty", nil])
+  end
 end
