d9b9533f553dbfbc6316b27d5bf92fdff8e76707
William Weidendorf <wweidendorf@gmail.com>
2014-01-23 18:22:23 -0500

    Can now toggle on and off favorite for user, item

--- app/models/favorite.rb
+++ app/models/favorite.rb
@@ -1,4 +1,17 @@
 class Favorite < ActiveRecord::Base
   belongs_to :item
   belongs_to :user
+
+  def self.toggle_for_user_and_item(user_id, item_id)
+    user  = User.find(user_id)
+    scope = user.favorites.where(item_id: item_id)
+
+    if scope.count > 0
+      scope.each(&:destroy)
+      false
+    else
+      scope.create
+      true
+    end
+  end
 end
