b7c459b4d6c793bfdcb4e5c427fe4a981516a258
William Weidendorf <wweidendorf@gmail.com>
2014-01-23 21:15:18 -0500

    Added cart items #create

---
+++ app/controllers/cart_items_controller.rb
@@ -0,0 +1,16 @@
+class CartItemsController < CatalogAbstractController
+  def create
+    current_cart.items.create(cart_item_params)
+    render(template: "carts/show")
+  end
+
+  private
+
+  def cart_item_params
+    ciparams = params.require(:cart_item).permit(:item_id, :quantity)
+    ciparams[:item_version] = Item.find(ciparams[:item_id]).version
+    ciparams[:item_options] = params[:cart_item][:item_options]
+
+    ciparams
+  end
+end
--- app/models/cart_item.rb
+++ app/models/cart_item.rb
@@ -2,4 +2,15 @@ class CartItem < ActiveRecord::Base
   acts_as_paranoid
 
   belongs_to :cart
+
+  validates :quantity, :item_id, :item_version, :cart_id, presence: true
+  validates :item_id, :item_version, :cart_id, numericality: true
+
+  before_save :validate_item_options
+
+  private
+
+  # TODO: Add item option validations
+  def validate_item_options
+  end
 end
