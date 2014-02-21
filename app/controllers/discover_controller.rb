73066be6dc84d9f533cd2847357bdc557effe7de
William Weidendorf <wweidendorf@gmail.com>
2014-02-21 18:03:43 -0500

    Added no results copy

--- app/controllers/discover_controller.rb
+++ app/controllers/discover_controller.rb
@@ -5,5 +5,7 @@ class DiscoverController < CatalogAbstractController
   def create
     @items_by_boutique_id = Item.discover(params).results.group_by(&:boutique_id)
     @boutiques            = Boutique.find(@items_by_boutique_id.keys).index_by(&:id)
+
+    render("no_results") if @items_by_boutique_id.blank?
   end
 end
