5077e1590d21bb72f1a2617b37380ff564161e55
William Weidendorf <wweidendorf@gmail.com>
2014-02-21 00:17:55 -0500

    Validate Address

--- app/models/address.rb
+++ app/models/address.rb
@@ -1,6 +1,8 @@
 class Address < ActiveRecord::Base
   belongs_to :parent, polymorphic: true
 
+  validates_presence_of :parent_id, :parent_type, :street, :city, :state, :postal_code
+
   def to_s
     [street, street2, "\n", city, state, postal_code].compact.join(" ")
   end
