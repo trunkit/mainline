ef940631e87d2da729df0693f8750e7b1346f759
William Weidendorf <wweidendorf@gmail.com>
2014-02-20 22:24:46 -0500

    Added initial discover views

--- app/controllers/discover_controller.rb
+++ app/controllers/discover_controller.rb
@@ -3,6 +3,7 @@ class DiscoverController < CatalogAbstractController
   end
 
   def create
-    @items = Item.discover(params)
+    @items_by_boutique_id = Item.discover(params).results.group_by(&:boutique_id)
+    @boutiques            = Boutique.find(@items_by_boutique_id.keys).index_by(&:id)
   end
 end
