cfc1bfce8a12bc6b501f8e0477c25708321f85a9
William Weidendorf <wweidendorf@gmail.com>
2014-03-08 14:31:17 -0500

    Replace solr with elasticsearch

--- app/models/item.rb
+++ app/models/item.rb
@@ -1,33 +1,8 @@
 class Item < ActiveRecord::Base
+  include Elasticsearch::Model
+
   acts_as_paranoid
   has_paper_trail
-
-  searchable do
-    text :name, :description, :fit, :construction, :model_measurements
-    text :categories do
-      categories.map(&:name)
-    end
-
-    text :boutique do
-      boutique.name
-    end
-
-    text :brand do
-      brand.name
-    end
-
-    text :cities do
-      boutique.locations.map(&:city)
-    end
-
-    text :states do
-      boutique.locations.map(&:state)
-    end
-
-    integer :boutique_id, :brand_id
-    integer :category_ids, multiple: true
-    double  :price
-  end
 
   belongs_to :parent, class_name: "Item"
   belongs_to :boutique, counter_cache: true
@@ -53,17 +28,13 @@ class Item < ActiveRecord::Base
   end
 
   def self.discover(params)
-    per_page = params[:per_page].to_i
-    per_page = 20 if per_page < 1 || per_page > 100
+    per = params[:per_page].to_i
+    per = 20 if per_page < 1 || per_page > 100
 
     page = params[:page].to_i
     page = 1 if page < 1
 
-    search do
-      fulltext(params[:q])
-
-      paginate(page: page, per_page: per_page)
-    end
+    search(params[:q]).page(page).per(per).records
   end
 
   def primary_photo
@@ -76,5 +47,17 @@ class Item < ActiveRecord::Base
 
   def version
     versions.count + 1
+  end
+
+  def as_indexed_json(options = {})
+    hsh = as_json(except: [:id])
+
+    hsh[:categories]    = categories.map(&:name)
+    hsh[:boutique_name] = boutique.name
+    hsh[:brand_name]    = brand.name
+    hsh[:cities]        = boutique.locations.map(&:city)
+    hsh[:states]        = boutique.locations.map(&:state)
+
+    hsh
   end
 end
