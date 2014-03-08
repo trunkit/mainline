b55eeb792d2ba699ff6b245f497601368dc7f6ad
William Weidendorf <wweidendorf@gmail.com>
2014-03-08 15:36:52 -0500

    Formatting

--- config/configatron/defaults.rb
+++ config/configatron/defaults.rb
@@ -1,14 +1,3 @@
-# Put all your default configatron settings here.
-
-# Example:
-#   configatron.emails.welcome.subject = 'Welcome!'
-#   configatron.emails.sales_reciept.subject = 'Thanks for your order'
-#
-#   configatron.file.storage = :s3
-
-configatron.s3.access_key_id     = "AKIAIVXNOE4PA6LP7BJQ"
-configatron.s3.secret_access_key = "5cp59VhNKi7T+c5jSxz6A0r7F98Ld0uqVd/dldNw"
-
 configatron.facebook do |f|
   f.id     = "652420558116466"
   f.secret = "6adb28b82b59e95760582f2bc5da3d3b"
@@ -17,6 +6,11 @@ end
 configatron.twitter do |t|
   t.consumer_key    = "QsBW1ibb2IW6JmWrQY9iw"
   t.consumer_secret = "V90sTc3Fw0sqGF25pc2qBV2SZEkNxbdEopzX2N8nk"
+end
+
+configatron.s3 do |s3|
+  s3.access_key_id     = "AKIAIVXNOE4PA6LP7BJQ"
+  s3.secret_access_key = "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"
 end
 
 configatron.stripe do |s|
--- config/configatron/production.rb
+++ config/configatron/production.rb
@@ -1,8 +1,3 @@
-# Override your default settings for the Production environment here.
-#
-# Example:
-#   configatron.file.storage = :s3
-
 configatron.stripe do |s|
   s.secret_key      = "sk_live_6zijLEFVjfDx1Sb7rPqXDmAD"
   s.publishable_key = "pk_live_M6YV7tsmI7rK8riMyhdkndBm"
