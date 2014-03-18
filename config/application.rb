6fb787c38a24c8d647a5198ebe6011f7238259cf
William Weidendorf <wweidendorf@gmail.com>
2014-03-18 14:08:53 -0400

    Added stubs for unit tests

--- config/application.rb
+++ config/application.rb
@@ -4,6 +4,7 @@ require File.expand_path('../boot', __FILE__)
 require "action_controller/railtie"
 require "active_record/railtie"
 require "action_mailer/railtie"
+require "rails/test_unit/railtie"
 require "sprockets/railtie"
 
 # Require the gems listed in Gemfile, including any gems
