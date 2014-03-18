787300299ea005809bb862680195ba203f56760b
William Weidendorf <wweidendorf@gmail.com>
2014-03-18 18:06:36 -0400

    Added some basic scopes to Activity

--- app/models/activity.rb
+++ app/models/activity.rb
@@ -1,4 +1,7 @@
 class Activity < ActiveRecord::Base
   belongs_to :owner,   polymorphic: true
   belongs_to :subject, polymorphic: true
+
+  scope :for_owner,   ->(owner)   { where(owner_id:   owner.id,   owner_type:   owner.class.to_s)   }
+  scope :for_subject, ->(subject) { where(subject_id: subject.id, subject_type: subject.class.to_s) }
 end
