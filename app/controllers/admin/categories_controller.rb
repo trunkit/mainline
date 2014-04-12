89268fadef07b5c99749b9bae3db8c524eb89c38
William Weidendorf <wweidendorf@gmail.com>
2014-04-12 17:41:58 -0400

    Can now add, update, and remove categories

---
+++ app/controllers/admin/categories_controller.rb
@@ -0,0 +1,48 @@
+class Admin::CategoriesController < ApplicationController
+  def index
+    @categories = Category.includes(:parent).all
+  end
+
+  def new
+    @category   = Category.new
+    @categories = Category.all.map{|c| [c.name, c.id] }.unshift(['(Top Level)', nil])
+  end
+
+  def create
+    @category = Category.new(category_params)
+
+    if @category.save
+      redirect_to([:edit, :admin, @category])
+    else
+      @categories = Category.all.map{|c| [c.name, c.id] }.unshift(['(Top Level)', nil])
+      render(action: :new)
+    end
+  end
+
+  def edit
+    @category   = Category.find(params[:id])
+    @categories = Category.all.map{|c| [c.name, c.id] }.unshift(['(Top Level)', nil])
+  end
+
+  def update
+    @category = Category.find(params[:id])
+
+    if @category.update_attributes(category_params)
+      redirect_to([:edit, :admin, @category])
+    else
+      @categories = Category.all.map{|c| [c.name, c.id] }.unshift(['(Top Level)', nil])
+      render(action: :edit)
+    end
+  end
+
+  def destroy
+    Category.find(params[:id]).destroy
+    redirect_to(:admin, :categories)
+  end
+
+private
+
+  def category_params
+    params.require(:category).permit(:parent_id, :name)
+  end
+end
--- app/models/category.rb
+++ app/models/category.rb
@@ -2,4 +2,6 @@ class Category < ActiveRecord::Base
   acts_as_tree
 
   has_and_belongs_to_many :items
+
+  validates :name, presence: true, uniqueness: true
 end
