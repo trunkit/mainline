4dfa348201d295f2612e2dafc8579f2953bdfebb
William Weidendorf <wweidendorf@gmail.com>
2014-04-12 19:01:35 -0400

    Remove errant scope on Brand#items relationship

--- app/models/brand.rb
+++ app/models/brand.rb
@@ -2,5 +2,5 @@ class Brand < ActiveRecord::Base
   acts_as_paranoid
   has_paper_trail
 
-  has_many :items, -> { where(parent_id: nil) }, dependent: :destroy
+  has_many :items, dependent: :destroy
 end
