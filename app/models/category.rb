a43fd660974406b1b775ec9b40f50b1ad047635d
William Weidendorf <wweidendorf@gmail.com>
2014-04-12 20:12:25 -0400

    Use param'd category name

--- app/models/category.rb
+++ app/models/category.rb
@@ -4,4 +4,8 @@ class Category < ActiveRecord::Base
   has_and_belongs_to_many :items
 
   validates :name, presence: true, uniqueness: true
+
+  def to_param
+    "#{id}-#{name.parameterize}"
+  end
 end
