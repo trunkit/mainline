69819bf65f299e2fefc598254513d0c48212ffe5
William Weidendorf <wweidendorf@gmail.com>
2014-04-16 21:05:54 -0400

    Can now manage full item photo lifecycle

--- app/controllers/item_photos_controller.rb
+++ app/controllers/item_photos_controller.rb
@@ -1,2 +1,16 @@
 class ItemPhotosController < ApplicationController
+  before_filter do
+    @item = current_user.items.find(params[:item_id])
+  end
+
+  def create
+    params[:photo][:url].each do |file|
+      @item.photos.create(url: file)
+    end
+  end
+
+  def destroy
+    @item.photos.find(params[:id]).destroy
+    render("create")
+  end
 end
--- app/models/item_photo.rb
+++ app/models/item_photo.rb
@@ -2,4 +2,6 @@ class ItemPhoto < ActiveRecord::Base
   belongs_to :item
 
   mount_uploader :url, StreamPhotoUploader
+
+  validates_presence_of :url
 end
