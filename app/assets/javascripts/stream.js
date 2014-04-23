20770bc8067bee53c80d0005823ef28336e23553
William Weidendorf <wweidendorf@gmail.com>
2014-04-23 19:36:24 -0400

    Change how the edit quantity interface works

--- app/assets/javascripts/stream.js
+++ app/assets/javascripts/stream.js
@@ -1,6 +1,26 @@
 SiteBindings.streamItems = function() {
   SiteBindings.item.quantities();
 
+  $("article.item").mouseleave(function() {
+    if($(this).find("form.item-quantity").is(":visible")) {
+      $(this).find(".edit.button").trigger("click");
+    }
+  });
+
+  $(".edit.button").click(function() {
+    quantityForm = $(this).parents("article.item").find("form.item-quantity");
+
+    if(quantityForm.is(":visible")) {
+      quantityForm.hide();
+      $(this).text("Edit");
+    } else {
+      quantityForm.show();
+      $(this).text("Close");
+    }
+
+    return false;
+  });
+
   $('form.item-quantity').on("ajax:beforeSend", function() {
     $.fancybox.showLoading();
   }).on("ajax:complete", function() {
