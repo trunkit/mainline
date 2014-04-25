9c5368ff87118f178ded03857189a305db9d5b01
William Weidendorf <wweidendorf@gmail.com>
2014-04-25 13:39:35 -0400

    Boutique signup won't have the auth token for CSRF

--- app/controllers/boutique_sign_up_controller.rb
+++ app/controllers/boutique_sign_up_controller.rb
@@ -1,4 +1,6 @@
 class BoutiqueSignUpController < ApplicationController
+  skip_before_action :verify_authenticity_token
+
   def create
     Notifier.boutique_signup(params[:boutique]).deliver
     render(inline: "<script>window.close()</script>")
