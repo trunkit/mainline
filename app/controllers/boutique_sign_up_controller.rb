6b18c2656c35e3c8d17c7a826e4e3cff7bf14bf1
William Weidendorf <wweidendorf@gmail.com>
2014-04-25 13:24:47 -0400

    Deliver the mail that we create

--- app/controllers/boutique_sign_up_controller.rb
+++ app/controllers/boutique_sign_up_controller.rb
@@ -1,6 +1,6 @@
 class BoutiqueSignUpController < ApplicationController
   def create
-    Notifier.boutique_signup(params[:boutique])
+    Notifier.boutique_signup(params[:boutique]).deliver
     render(inline: "<script>window.close()</script>")
   end
 end
