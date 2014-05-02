7a995dc3e44ca16f03b08feb1b50b3ff7f38319b
William Weidendorf <wweidendorf@gmail.com>
2014-05-01 21:23:29 -0400

    Added configurable weight to items

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20140502011645) do
+ActiveRecord::Schema.define(version: 20140502012123) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -125,6 +125,7 @@ ActiveRecord::Schema.define(version: 20140502011645) do
     t.boolean  "approved",                                      default: false, null: false
     t.integer  "primary_category_id"
     t.integer  "secondary_category_id"
+    t.decimal  "weight",                precision: 5, scale: 2, default: 1.0
   end
 
   create_table "locations", force: true do |t|
