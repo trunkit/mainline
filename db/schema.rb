07da948e35770394dc4e29415e9f356b7054fce3
William Weidendorf <wweidendorf@gmail.com>
2014-05-15 03:52:39 -0400

    Remove locations

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20140515074144) do
+ActiveRecord::Schema.define(version: 20140515075209) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -144,22 +144,6 @@ ActiveRecord::Schema.define(version: 20140515074144) do
     t.decimal  "packaging_length",      precision: 9, scale: 2, default: 12.0
     t.text     "parcel_id"
   end
-
-  create_table "locations", force: true do |t|
-    t.string   "name"
-    t.hstore   "hours"
-    t.datetime "deleted_at"
-    t.datetime "created_at"
-    t.datetime "updated_at"
-    t.boolean  "primary",      default: false
-    t.string   "cover_photo"
-    t.string   "stream_photo"
-    t.integer  "company_id"
-    t.string   "company_type"
-    t.string   "phone_number"
-  end
-
-  add_index "locations", ["company_id", "company_type"], name: "index_locations_on_company_id_and_company_type", using: :btree
 
   create_table "user_invite_tokens", force: true do |t|
     t.integer  "parent_id"
