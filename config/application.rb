00f886a0032d73090483cf5ab28da9185a5c3059
William Weidendorf <wweidendorf@gmail.com>
2014-05-18 00:09:55 -0400

    Instrument elasticsearch

--- config/application.rb
+++ config/application.rb
@@ -6,6 +6,7 @@ require "active_record/railtie"
 require "action_mailer/railtie"
 require "rails/test_unit/railtie"
 require "sprockets/railtie"
+require "elasticsearch/rails/instrumentation"
 
 # Require the gems listed in Gemfile, including any gems
 # you've limited to :test, :development, or :production.
