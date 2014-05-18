75d6fe952c613b3abbbd901a25de550ecf2b6089
William Weidendorf <wweidendorf@gmail.com>
2014-05-18 00:05:41 -0400

    Can notify objects when an activity event has been created about them

--- app/models/activity.rb
+++ app/models/activity.rb
@@ -6,4 +6,9 @@ class Activity < ActiveRecord::Base
 
   scope :for_owner,   ->(owner)   { where(owner_id:   owner.id,   owner_type:   owner.class.to_s)   }
   scope :for_subject, ->(subject) { where(subject_id: subject.id, subject_type: subject.class.to_s) }
+
+  after_save do
+    owner.notify(action, subject) if owner.respond_to?(:notify)
+    subject.notify(action, owner) if subject.respond_to?(:notify)
+  end
 end
