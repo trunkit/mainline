f7bb82b171613fa2498fdb81bacb29f87ed4a6a8
William Weidendorf <wweidendorf@gmail.com>
2014-05-27 21:32:15 -0400

    Disabled the configatron live key for beta testing purposes

--- config/configatron/production.rb
+++ config/configatron/production.rb
@@ -3,7 +3,7 @@ configatron.easypost do |e|
   e.publishable_key = ""
 end
 
-configatron.stripe do |s|
-  s.secret_key      = "sk_live_6zijLEFVjfDx1Sb7rPqXDmAD"
-  s.publishable_key = "pk_live_M6YV7tsmI7rK8riMyhdkndBm"
-end
+# configatron.stripe do |s|
+#   s.secret_key      = "sk_live_6zijLEFVjfDx1Sb7rPqXDmAD"
+#   s.publishable_key = "pk_live_M6YV7tsmI7rK8riMyhdkndBm"
+# end
