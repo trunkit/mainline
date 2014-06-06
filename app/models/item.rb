2a33c98f0097ca825201474c1e80fb8e9279f441
William Weidendorf <wweidendorf@gmail.com>
2014-06-06 13:03:52 -0400

    Item.discover should return results in order of relevance

--- app/models/item.rb
+++ app/models/item.rb
@@ -83,8 +83,8 @@ class Item < ActiveRecord::Base
 
       items = search(query).page(page).per(per).records.includes(:boutique)
 
-      activities = item_activity_scope(user).where(subject_id: items.map(&:id))
-      activities.map{|activity| [activity, items.detect{|i| i.id == activity.subject_id }] }
+      activities = item_activity_scope(user).where(subject_id: items.map(&:id)).index_by(&:subject_id)
+      items.map{|i| [activities[i.id], i] }
     end
 
   private
