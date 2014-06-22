15b9f81476de4718bee5a1311ef5460b94e4aa0e
William Weidendorf <wweidendorf@gmail.com>
2014-06-22 12:39:03 -0400

    Updated notifications display

--- app/helpers/notifications_helper.rb
+++ app/helpers/notifications_helper.rb
@@ -10,4 +10,27 @@ module NotificationsHelper
       render("boutique_follow", notification: notification)
     end
   end
+
+  def notification_time_in_words(notification)
+    to_time   = Time.now
+    from_time = notification.created_at
+
+    distance_in_minutes = ((to_time - from_time)/60.0).round
+    distance_in_seconds = (to_time - from_time).round
+
+    text = case distance_in_minutes
+    when 0..1
+      "#{distance_in_seconds}s"
+    when 2...60
+      "#{distance_in_minutes}m"
+    when 60...1440
+      "#{(distance_in_minutes / 60.0).round}h"
+    when 1440...43200
+      "#{(distance_in_minutes / 1440.0).round}d"
+    else
+      from_time.strftime("%m/%d/%Y")
+    end
+
+    content_tag(:span, text, class: "timestamp")
+  end
 end
