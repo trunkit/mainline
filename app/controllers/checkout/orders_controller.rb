2de5ae1c7093a7a1711ec9f858b06ecb02c42abf
William Weidendorf <wweidendorf@gmail.com>
2014-06-22 18:00:49 -0400

    Checkout process now properly presents and handles trunkit credit

--- app/controllers/checkout/orders_controller.rb
+++ app/controllers/checkout/orders_controller.rb
@@ -2,10 +2,12 @@ class Checkout::OrdersController < CatalogAbstractController
   force_ssl if: -> { Rails.env.production? }
 
   before_filter do
-    begin
-      @token = Stripe::Token.retrieve(session[:card_token])
-    rescue Stripe::InvalidRequestError
-      redirect_to(checkout_payment_method_path)
+    if current_cart.total_price_after_credit > 0
+      begin
+        @token = Stripe::Token.retrieve(session[:card_token])
+      rescue Stripe::InvalidRequestError
+        redirect_to(checkout_payment_method_path)
+      end
     end
   end
 
--- app/controllers/checkout/payment_methods_controller.rb
+++ app/controllers/checkout/payment_methods_controller.rb
@@ -2,6 +2,7 @@ class Checkout::PaymentMethodsController < CatalogAbstractController
   force_ssl if: -> { Rails.env.production? }
 
   def show
+    redirect_to(checkout_order_path) unless current_cart.total_price_after_credit > 0
   end
 
   def update
