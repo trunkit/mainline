309677f5ac91671910e2eaa1966e1b85ca93ddf1
William Weidendorf <wweidendorf@gmail.com>
2014-06-22 15:27:37 -0400

    Order item action buttons are now more robust

--- app/helpers/orders_helper.rb
+++ app/helpers/orders_helper.rb
@@ -1,2 +1,13 @@
 module OrdersHelper
+  def item_order_status_button(cart_item)
+    if cart_item.refund_id.present?
+      link_to("Refunded", "#", class: "btn btn-supplied disabled")
+    elsif cart_item.completed_at.present?
+      link_to("Shipping Label", "#", target: "_blank", class: "btn")
+    elsif cart_item.supplied?(current_user.parent_id)
+      link_to("Complete", "#cart-item-complete-#{cart_item.id}", class: "btn fancybox")
+    else
+      link_to("Supported!", "#", class: "btn btn-supplied")
+    end
+  end
 end
