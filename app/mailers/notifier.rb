5b671b87c47fc60d4fb90366e4368deb44c2fc00
William Weidendorf <wweidendorf@gmail.com>
2014-06-22 15:27:12 -0400

    Added basic mailer for item cancellation

--- app/mailers/notifier.rb
+++ app/mailers/notifier.rb
@@ -15,4 +15,15 @@ class Notifier < ActionMailer::Base
       subject: "[TrunkIt] Boutique Signup"
     })
   end
+
+  def cart_item_cancellation(cart_item_id, message)
+    @cart_item = CartItem.includes(:cart).find(cart_item_id)
+    @user      = User.unscoped.find(@cart_item.cart.user_id)
+    @message   = message
+
+    mail({
+      to: @user.email,
+      subject: "Order Update for #{@cart_item.item.name} from #{@cart_item.supporting_boutique.name}"
+    })
+  end
 end
