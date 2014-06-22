45c85860a65a010dc40e62fa1967c231b3ca76ce
William Weidendorf <wweidendorf@gmail.com>
2014-06-22 17:28:54 -0400

    More cart item states for boutique admin

--- app/helpers/orders_helper.rb
+++ app/helpers/orders_helper.rb
@@ -2,6 +2,10 @@ module OrdersHelper
   def item_order_status_button(cart_item)
     if cart_item.cancellation_refund_id.present?
       link_to("Cancelled", "#", class: "btn btn-supplied disabled")
+    elsif cart_item.refund_ledger_entry_id.present?
+      link_to("Refunded", "#", target: "_blank", class: "btn disabled")
+    elsif cart_item.refund_requested.present?
+      link_to("Refund Requested", "#", target: "_blank", class: "btn")
     elsif cart_item.completed_at.present?
       link_to("Shipping Label", "#", target: "_blank", class: "btn")
     elsif cart_item.supplied?(current_user.parent_id)
