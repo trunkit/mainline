10aac40670ce16ebe4468c243a11054f45fb7e40
William Weidendorf <wweidendorf@gmail.com>
2014-06-22 13:51:28 -0400

    Use real customer data for orders listing address

--- app/controllers/orders_controller.rb
+++ app/controllers/orders_controller.rb
@@ -1,5 +1,6 @@
 class OrdersController < CatalogAbstractController
   def index
     @orders = Cart.boutique_orders_listing(current_user, params)
+    @users  = User.unscoped.find(@orders.map(&:user_id)).index_by(&:id)
   end
 end
