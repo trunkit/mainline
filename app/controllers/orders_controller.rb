a7b9cbd819ad2bfab49b72a21760fb78948918df
William Weidendorf <wweidendorf@gmail.com>
2014-06-30 22:30:25 -0400

    Added front-end for shopper refund lifecycle

--- app/controllers/orders_controller.rb
+++ app/controllers/orders_controller.rb
@@ -1,11 +1,15 @@
 class OrdersController < CatalogAbstractController
-  before_filter do
-    redirect_to(stream_path) unless current_user.parent_id.present?
-  end
-
   def index
-    @orders      = Cart.boutique_orders_listing(current_user, params)
-    @users       = User.unscoped.find(@orders.map(&:user_id)).index_by(&:id)
-    @commissions = Commission.collectable_for(current_user)
+    if current_user.parent_id.blank?
+      @orders = Cart.where(user_id: current_user.id).ordered.includes(:user)
+
+      render(action: "index_shopper")
+    else
+      @orders      = Cart.boutique_orders_listing(current_user, params)
+      @users       = User.unscoped.find(@orders.map(&:user_id)).index_by(&:id)
+      @commissions = Commission.collectable_for(current_user)
+
+      render(action: "index_boutique")
+    end
   end
 end
