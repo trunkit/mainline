59c0883b65a078fbbbed19829dc6568e4d2d0ab3
William Weidendorf <wweidendorf@gmail.com>
2014-06-30 22:06:32 -0400

    Only boutique users should have access to Orders

--- app/controllers/orders_controller.rb
+++ app/controllers/orders_controller.rb
@@ -1,4 +1,8 @@
 class OrdersController < CatalogAbstractController
+  before_filter do
+    redirect_to(stream_path) unless current_user.parent_id.present?
+  end
+
   def index
     @orders = Cart.boutique_orders_listing(current_user, params)
     @users  = User.unscoped.find(@orders.map(&:user_id)).index_by(&:id)
