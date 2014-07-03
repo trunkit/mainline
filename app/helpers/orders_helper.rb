7b1ec648a69ce3a9d5b1a5e4c136cfa20a185d36
William Weidendorf <wweidendorf@gmail.com>
2014-07-02 21:19:02 -0400

    Styling tweaks

--- app/helpers/orders_helper.rb
+++ app/helpers/orders_helper.rb
@@ -7,11 +7,11 @@ module OrdersHelper
     elsif cart_item.refund_requested.present?
       link_to("Refund Requested", "#", target: "_blank", class: "btn")
     elsif cart_item.completed_at.present? && user.parent_id.present?
-      link_to("Shipping Label", cart_item.shipping_label.postage_label.label_url, target: "_blank", class: "btn")
+      link_to("Shipping Label", cart_item.shipping_label.postage_label.label_url, target: "_blank", class: "btn shipping-label")
     elsif cart_item.supplied?(current_user.parent_id)
-      link_to("Complete", "#cart-item-complete-#{cart_item.id}", class: "btn fancybox")
+      link_to("Pending", "#cart-item-complete-#{cart_item.id}", class: "btn fancybox")
     elsif user.parent_id.present?
-      link_to("Curated", "#", class: "btn btn-supplied")
+      link_to("Curated", "#", class: "btn btn-supplied disabled")
     else
       link_to("Request Refund", "#", class: "btn btn-supplied")
     end
