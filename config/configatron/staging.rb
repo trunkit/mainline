7d8b404735f61ec19476c723c75761a98d301dd2
William Weidendorf <wweidendorf@gmail.com>
2014-07-07 20:43:45 -0400

    Added staging specific config

---
+++ config/configatron/staging.rb
@@ -0,0 +1,9 @@
+configatron.facebook do |f|
+  f.id     = "834679196557267"
+  f.secret = "8b3deda0058e444f69119573373e9764"
+end
+
+configatron.twitter do |t|
+  t.consumer_key    = "7xML0J1SPrJyO2DHeE1EA"
+  t.consumer_secret = "qi25RIMVgzlow9cGfh2dWUz0MbO4cI2kvBxWV42ig"
+end
