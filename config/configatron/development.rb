2e709199de8e09e090643ac164f7cdec4711f2ad
William Weidendorf <wweidendorf@gmail.com>
2014-07-07 20:43:40 -0400

    Useless comment

--- config/configatron/development.rb
+++ config/configatron/development.rb
@@ -1,8 +1,3 @@
-# Override your default settings for the Development environment here.
-#
-# Example:
-#   configatron.file.storage = :local
-
 configatron.facebook do |f|
   f.id     = "159347764265403"
   f.secret = "b27a5e0c8e0c1c8a118acfb1e92875bc"
