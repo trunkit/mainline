47aed09d54e03b5818977665a67f52701124252b
William Weidendorf <wweidendorf@gmail.com>
2014-07-19 16:32:54 -0400

    Make stream an infinite scroll

--- app/assets/javascripts/stream.js
+++ app/assets/javascripts/stream.js
@@ -1,31 +1,53 @@
-SiteBindings.streamItems = function() {
-  SiteBindings.item.quantities();
+SiteBindings.stream = {
+  items: function() {
+    SiteBindings.item.quantities();
 
-  $("article.item").mouseleave(function() {
-    if($(this).find("form.item-quantity").is(":visible")) {
-      $(this).find(".edit.button").trigger("click");
-    }
-  });
+    $(document).on("article.item", "mouseleave", function() {
+      if($(this).find("form.item-quantity").is(":visible")) {
+        $(this).find(".edit.button").trigger("click");
+      }
+    });
 
-  $(".edit.button").click(function() {
-    quantityForm = $(this).parents("article.item").find("form.item-quantity");
+    $(".edit.button").click(function() {
+      quantityForm = $(this).parents("article.item").find("form.item-quantity");
 
-    if(quantityForm.is(":visible")) {
-      quantityForm.hide();
-      $(this).text("Edit");
-    } else {
-      quantityForm.show();
-      $(this).text("Close");
-    }
+      if(quantityForm.is(":visible")) {
+        quantityForm.hide();
+        $(this).text("Edit");
+      } else {
+        quantityForm.show();
+        $(this).text("Close");
+      }
 
-    return false;
-  });
+      return false;
+    });
 
-  $('form.item-quantity').on("ajax:beforeSend", function() {
-    $.fancybox.showLoading();
-  }).on("ajax:complete", function() {
-    $.fancybox.hideLoading();
-    $(this).find("input[type=text]").removeAttr("disabled");
-    $(this).children("input[type=hidden]").remove();
-  });
+    $('form.item-quantity').on("ajax:beforeSend", function() {
+      $.fancybox.showLoading();
+    }).on("ajax:complete", function() {
+      $.fancybox.hideLoading();
+      $(this).find("input[type=text]").removeAttr("disabled");
+      $(this).children("input[type=hidden]").remove();
+    });
+  },
+  watcher: function(nextURL) {
+    $("#stream-watcher").bind("inview", function( event, isInView, visiblePartX, visiblePartY ) {
+      if(nextURL === "" || typeof nextURL === "undefined")
+        return;
+
+      if(visiblePartY == 'both') {
+        $.fancybox.showLoading();
+        $("#stream-watcher").unbind("inview");
+
+        $.ajax({
+          dataType: "script",
+          type: "GET",
+          url: nextURL,
+          complete: function() {
+            $.fancybox.hideLoading();
+          }
+        });
+      }
+    });
+  }
 };
