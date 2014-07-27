9043eef97ad72b5787dd37a5c63aa4153e2857f4
William Weidendorf <wweidendorf@gmail.com>
2014-07-26 20:36:33 -0400

    Initial admin order listing interface

--- app/controllers/admin/orders_controller.rb
+++ app/controllers/admin/orders_controller.rb
@@ -1,2 +1,6 @@
 class Admin::OrdersController < Admin::AbstractController
+  def index
+    @orders = Cart.where.not(captured_at: nil).includes(:items).order(created_at: :desc)
+    @users  = User.unscoped.where(id: @orders.map(&:user_id).uniq).index_by(&:id)
+  end
 end
