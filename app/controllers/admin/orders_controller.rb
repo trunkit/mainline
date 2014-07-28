db048ee7cde59bfad27effc8d7c51e15ffd08763
William Weidendorf <wweidendorf@gmail.com>
2014-07-28 00:03:35 -0400

    Initial, basic super admin orders interface

--- app/controllers/admin/orders_controller.rb
+++ app/controllers/admin/orders_controller.rb
@@ -3,4 +3,9 @@ class Admin::OrdersController < Admin::AbstractController
     @orders = Cart.where.not(captured_at: nil).includes(:items).order(created_at: :desc)
     @users  = User.unscoped.where(id: @orders.map(&:user_id).uniq).index_by(&:id)
   end
+
+  def show
+    @cart = Cart.includes(:items).find(params[:id])
+    @user = User.unscoped.find(@cart.user_id)
+  end
 end
