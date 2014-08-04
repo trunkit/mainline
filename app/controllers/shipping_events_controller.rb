03d0d934004c891063687ad3a2c3f8137c026b6c
William Weidendorf <wweidendorf@gmail.com>
2014-08-03 21:18:49 -0400

    Capture shipping events

--- app/controllers/shipping_events_controller.rb
+++ app/controllers/shipping_events_controller.rb
@@ -1,2 +1,16 @@
 class ShippingEventsController < ApplicationController
+  def create
+    e = EasyPost::Event.receive(request.body.string)
+
+    ShippingEvent.create({
+      event_id:            e.id,
+      object:              e.object,
+      mode:                e.mode,
+      description:         e.description,
+      previous_attributes: e.previous_attributes,
+      result:              e.result
+    })
+
+    render(nothing: true, status: 200)
+  end
 end
--- app/models/shipping_event.rb
+++ app/models/shipping_event.rb
@@ -1,2 +1,27 @@
 class ShippingEvent < ActiveRecord::Base
+  after_save :check_shipped, :check_delivered
+
+  def result
+    @result ||= Hashie::Mash.new(result)
+  end
+
+private
+
+  def check_shipped
+    return unless status == "tracker.updated"
+
+    if status == "in_transit"
+      ci = CartItem.where(tracking_code: result.tracking_code).first
+      ci.update_attribute(:shipped, true) if ci
+    end
+  end
+
+  def check_delivered
+    return unless status == "tracker.updated"
+
+    if status == "delivered"
+      ci = CartItem.where(tracking_code: result.tracking_code).first
+      ci.update_attribute(:delivered, true) if ci
+    end
+  end
 end
