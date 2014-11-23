261b9517635af85928919bc0ca5ab30a3a12f1d4
William Weidendorf <wweidendorf@gmail.com>
2014-11-23 14:19:14 -0500

    Added basic Swagger API descriptor

--- app/controllers/stream_controller.rb
+++ app/controllers/stream_controller.rb
@@ -1,4 +1,18 @@
 class StreamController < CatalogAbstractController
+  swagger_controller :stream, "Product Stream"
+
+  swagger_api :index do
+    summary "Fetches all items in stream"
+    notes "The return output is paginated"
+
+    param :query, :category, :integer, :optional, "Category ID"
+    param :query, :boutique_id, :integer, :optional, "Boutique ID"
+
+    response :unauthorized
+    response :not_acceptable
+    response :unprocessable_entity
+  end
+
   def index
     @activity_items = Item.for_stream(current_user, params)
 
